6f83df7492586212b8130d92cf7479b0e5daaa52
James Wilson <jsw.james.wilson@gmail.com>
2014-02-12 17:25:39 -0800

    Fully functional Chrome Extension. Doesn't remember selection yet.

---
+++ highlighter.js
@@ -0,0 +1,15 @@
+var highlightListener = function(){
+  var word;
+  document.onmouseup = function(){
+    jQuery('body').unhighlight();
+    word = '';
+    var selection = window.getSelection();
+    for (var i = 0; i < selection.rangeCount; i++){
+      word += selection.getRangeAt(i);
+    }
+    jQuery('html').highlight(word, {wordsOnly: true});
+    $('.highlight').css({'background-color':'yellow'});
+  };
+};
+
+highlightListener();
