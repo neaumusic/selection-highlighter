9f91281499a27b2d7ed10b1ac5389e305ca8b6c6
James Wilson <jsw.james.wilson@gmail.com>
2014-02-13 19:38:01 -0800

    Cleaned up code. I still can't find a stable way to maintain selection after highlight.

--- highlighter.js
+++ highlighter.js
@@ -1,15 +1,22 @@
+/*
+ *
+ *  jquery.highlight.js served under MIT license from https://github.com/bartaz/sandbox.js
+ *
+ */
+
+(function($){
+
 var highlightListener = function(){
-  var word;
-  document.onmouseup = function(){
+  jQuery(document).on('mouseup', function(e){
     jQuery('body').unhighlight();
-    word = '';
-    var selection = window.getSelection();
-    for (var i = 0; i < selection.rangeCount; i++){
-      word += selection.getRangeAt(i);
+    var word = '' + window.getSelection().getRangeAt(0);
+    if (word.length && word !== ' '){
+      jQuery('html').highlight(word, {wordsOnly: true});
+      $('.highlightSel').css({'background-color':'yellow'});
     }
-    jQuery('html').highlight(word, {wordsOnly: true});
-    $('.highlightSel').css({'background-color':'yellow'});
-  };
+  });
 };
 
 highlightListener();
+
+}).call(this, jQuery);
