719951dc66ede360f77e2dbfd44d4e21d46df29b
James Wilson <jsw.james.wilson@gmail.com>
2016-05-17 10:05:58 -0700

    Prevent style/script manipulation, debounce on drags

--- highlighter.js
+++ highlighter.js
@@ -3,42 +3,51 @@ var highlightedSpanTemplate = document.createElement("span");
     highlightedSpanTemplate.className = "highlighted_selection";
     highlightedSpanTemplate.style.backgroundColor = "yellow";
 
-window.addEventListener("mouseup", handleMouseUp);
+document.addEventListener("selectionchange", handleSelectionChange);
 
-function handleMouseUp () {
+function handleSelectionChange (e) {
     removeAllHighlights();
 
     var selection = window.getSelection(),
-        selectionString = selection + ""; // same as .toString()
+        selectionString = (selection + "").trim(); // same as .toString()
 
-    if (selectionString + "" === "" || selectionString + "" === " ") {
+    if (selectionString === "" || selectionString === " ") {
         return; // empty selection
     } else if (selection.anchorNode !== selection.focusNode) {
         return; // selection crosses textNodes
+    } else if (selection.type === "None") {
+        return;
     }
-    
     var allTextNodes = getAllTextNodes(document.body),
         currentTextNode,
-        match;
+        match,
+        regex = new RegExp(selectionString);
 
-    requestAnimationFrame(function () {
+    requestAnimationFrame(debounce(function () {
+        document.removeEventListener("selectionchange", handleSelectionChange);
         for (var i = 0; i < allTextNodes.length; i++) {
             currentTextNode = allTextNodes[i];
-
-            if (match = currentTextNode.data.match(selectionString)) {
-                if (selection.anchorNode !== currentTextNode) {
+            parentNodeName = currentTextNode.parentNode.nodeName;
+            if (parentNodeName !== "SCRIPT" && parentNodeName !== "STYLE") {
+                if (match = currentTextNode.data.match(regex)) {
                     var isolatedTextNode = currentTextNode.splitText(match.index); // remove preceding
-                        isolatedTextNode.splitText(selectionString.length); // remove trailing
+                        allTextNodes.push(isolatedTextNode.splitText(selectionString.length)); // remove & save trailing
+                    if ((selection.anchorNode !== currentTextNode || selection.anchorOffset !== match.index) &&
+                        (selection.focusNode !== currentTextNode || selection.focusOffset !== match.index)) {
 
-                    var clonedStyledSpan = highlightedSpanTemplate.cloneNode(true);
-                        clonedStyledSpan.appendChild(isolatedTextNode.cloneNode(true));
+                        var clonedStyledSpan = highlightedSpanTemplate.cloneNode(true);
+                            clonedStyledSpan.appendChild(isolatedTextNode.cloneNode(true));
 
-                    isolatedTextNode.parentNode.insertBefore(clonedStyledSpan, isolatedTextNode);
-                    isolatedTextNode.parentNode.removeChild(isolatedTextNode);
+                        isolatedTextNode.parentNode.insertBefore(clonedStyledSpan, isolatedTextNode);
+                        isolatedTextNode.parentNode.removeChild(isolatedTextNode);
+                    }
                 }
             }
         }
-    });
+        setTimeout(function () {
+            document.addEventListener("selectionchange", handleSelectionChange);
+        }, 0);
+    }), 300);
 }
 
 function removeAllHighlights () {
@@ -59,3 +68,18 @@ function getAllTextNodes (rootNode) {
 
     return allTextNodes;
 } // credit http://stackoverflow.com/a/10730777/1487102
+
+function debounce (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+} // credit to underscore.js
