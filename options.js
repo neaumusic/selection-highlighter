0c8f433a28617295f64f358737497904715e5627
James Wilson <jwilson@uievolution.com>
2017-10-29 23:38:52 -0700

    add options.html with user-submitted JavaScript

---
+++ options.js
@@ -0,0 +1,45 @@
+
+// ------------------------------------------------------
+//            see highlighter.js userOptions
+// ------------------------------------------------------
+const defaultOptions = `({
+  highlightedClassName: 'highlighted_selection',
+  styles: {
+    display: 'inline',
+    backgroundColor: 'yellow',
+  },
+  isWindowLocationValid: function (windowLocation) {
+    // eg. return (windowLocation.host.includes('linkedin.com') === false);
+    return true;
+  },
+  isAncestorNodeValid: (
+    function isAncestorNodeValid (ancestorNode) {
+      return (
+        (!ancestorNode) ||
+        // eg. (!ancestorNode.classList || !ancestorNode.classList.contains('CodeMirror')) &&
+        (ancestorNode.nodeName !== 'SCRIPT') &&
+        (ancestorNode.nodeName !== 'STYLE') &&
+        (ancestorNode.nodeName !== 'HEAD') &&
+        (ancestorNode.nodeName !== 'INPUT') &&
+        (ancestorNode.nodeName !== 'TEXTAREA') &&
+        (ancestorNode.contentEditable !== 'true') &&
+        (isAncestorNodeValid(ancestorNode.parentNode))
+      );
+    }
+  ),
+})`;
+
+const optionsTextArea = document.querySelector('textarea#options')
+chrome.storage.sync.get('optionsText', e => {
+  optionsTextArea.value = (e.optionsText || defaultOptions);
+});
+
+document.querySelector('button#submit-button').addEventListener('click', e => {
+  chrome.storage.sync.set({ optionsText: optionsTextArea.value, });
+});
+
+document.querySelector('button#reset-button').addEventListener('click', e => {
+  if (window.confirm('Are you sure? Sorry for asking..')) {
+    chrome.storage.sync.clear(() => window.location.reload());
+  }
+});
