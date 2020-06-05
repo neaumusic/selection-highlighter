de634224a720b82483d5cd1026c4e5e2b8795d86
neaumusic <jsw.james.wilson@gmail.com>
2020-06-05 16:27:46 -0700

    bundle chrome extension with webpack

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 11,
+  },
+  'rules': {
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const RemoveFilesWebpackPlugin = require('remove-files-webpack-plugin');
+
+const ROOT = `dist/chrome-extension`;
+const OPTIONS_PAGE_PATH = `options_page`;
+const HIGHLIGHTER_PATH = `highlighter`;
+const ASSETS_PATH = `assets`;
+
+const manifest = {
+  entry: './src/chrome_extension/manifest.json',
+  output: {
+    path: path.resolve(__dirname, ROOT),
+    filename: 'DELETED.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'LICENSE.md'},
+        {
+          from: 'src/chrome_extension/manifest.json',
+          transform: function(manifestBuffer, path) {
+            const manifestString = manifestBuffer.toString()
+                .replace(/\$\{OPTIONS_PAGE_PATH\}/g, OPTIONS_PAGE_PATH)
+                .replace(/\$\{HIGHLIGHTER_PATH\}/g, HIGHLIGHTER_PATH)
+                .replace(/\$\{ASSETS_PATH\}/g, ASSETS_PATH);
+            return Buffer.from(manifestString);
+          },
+        },
+        {from: 'assets/icon.png', to: ASSETS_PATH},
+      ],
+    }),
+    new RemoveFilesWebpackPlugin({
+      after: {
+        log: false,
+        include: [
+          `${ROOT}/DELETED.js`,
+        ],
+      },
+    }),
+  ],
+  stats: true,
+  mode: 'none',
+};
+
+const highlighter = {
+  entry: './src/highlighter/highlighter.js',
+  output: {
+    path: path.resolve(__dirname, `${ROOT}/${HIGHLIGHTER_PATH}`),
+    filename: 'highlighter.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: [
+          {loader: 'babel-loader'},
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  stats: true,
+  mode: 'none',
+};
+
+const options_page = {
+  entry: './src/options_page/options_page.js',
+  output: {
+    path: path.resolve(__dirname, `${ROOT}/${OPTIONS_PAGE_PATH}`),
+    filename: 'options_page.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: [
+          {loader: 'babel-loader'},
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {loader: 'html-loader'},
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      filename: 'options_page.html',
+      template: './src/options_page/options_page.html',
+      title: 'Selection Highlighter Options',
+    }),
+  ],
+  stats: true,
+  mode: 'none',
+};
+
+module.exports = [
+  manifest,
+  highlighter,
+  options_page,
+];
+
